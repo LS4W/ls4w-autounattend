c44609743304eb1ea13ce30c0e66faf7e9aae76d
Skyl3r <kpereksta@gmail.com>
2020-11-18 13:58:27 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using WindowsBuilder.WindowsUnattendObject;
+using CommandLine;
+
+namespace WindowsBuilder
+{
+    class Program
+    {
+
+        public class Options {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            string language = "EN-US";
+            string locale = "0409:00000409";
+
+            Unattend unattendObject = new Unattend();
+
+            Settings windowsPESettings  = new Settings();
+            Settings oobeSettings       = new Settings();
+            Settings specializeSettings = new Settings();
+
+            windowsPESettings.Pass  = "windowsPE";
+            oobeSettings.Pass       = "oobeSystem";
+            specializeSettings.Pass = "specialize";
+            
+            /// SETUP UI COMPONENT
+
+            Component setupUIComponent = new Component();
+            setupUIComponent.SetupUILanguage = new SetupUILanguage();
+            setupUIComponent.SetupUILanguage.UILanguage = "EN-US";
+
+            setupUIComponent.InputLocale    = locale;
+            setupUIComponent.SystemLocale   = language;
+            setupUIComponent.UILanguage     = language;
+            setupUIComponent.UserLocale     = language;
+
+            windowsPESettings.Component = new System.Collections.Generic.List<Component>();
+
+
+            /// SETUP DISK COMPONENT
+
+            Component setupDiskComponent = new Component();
+            setupDiskComponent.DiskConfiguration = new DiskConfiguration();
+            setupDiskComponent.DiskConfiguration.Disk = new Disk();
+            setupDiskComponent.DiskConfiguration.Disk.CreatePartitions = new CreatePartitions();
+            setupDiskComponent.DiskConfiguration.Disk.ModifyPartitions = new ModifyPartitions();
+            setupDiskComponent.DiskConfiguration.Disk.CreatePartitions.CreatePartition = new System.Collections.Generic.List<CreatePartition>();
+            setupDiskComponent.DiskConfiguration.Disk.ModifyPartitions.ModifyPartition = new System.Collections.Generic.List<ModifyPartition>();
+
+            CreatePartition primaryPartition    = new CreatePartition();
+            CreatePartition efiPartition        = new CreatePartition();
+            CreatePartition msrPartition        = new CreatePartition();
+            CreatePartition windowsPartition    = new CreatePartition();
+
+            primaryPartition.Order  = "1";
+            primaryPartition.Size   = "450";
+            primaryPartition.Type   = "Primary";
+
+            efiPartition.Order      = "2";
+            efiPartition.Size       = "100";
+            efiPartition.Type       = "EFI";
+
+            msrPartition.Order      = "3";
+            msrPartition.Size       = "16";
+            msrPartition.Type       = "MSR";
+
+            windowsPartition.Order  = "4";
+            windowsPartition.Extend = "true";
+            windowsPartition.Type   = "Primary";
+
+            setupDiskComponent.DiskConfiguration.Disk.CreatePartitions.CreatePartition.Add(primaryPartition);
+            setupDiskComponent.DiskConfiguration.Disk.CreatePartitions.CreatePartition.Add(efiPartition);
+            setupDiskComponent.DiskConfiguration.Disk.CreatePartitions.CreatePartition.Add(msrPartition);
+            setupDiskComponent.DiskConfiguration.Disk.CreatePartitions.CreatePartition.Add(windowsPartition);
+
+            /// Setup Modify partitions
+
+            ModifyPartition primaryModifyPartition  = new ModifyPartition();
+            ModifyPartition efiModifyPartition      = new ModifyPartition();
+            ModifyPartition msrModifyPartition      = new ModifyPartition();
+            ModifyPartition windowsModifyPartition  = new ModifyPartition();
+
+            primaryModifyPartition.Format       = "NTFS";
+            primaryModifyPartition.Label        = "WinRE";
+            primaryModifyPartition.Order        = "1";
+            primaryModifyPartition.PartitionID  = "1";
+            primaryModifyPartition.TypeID       = "DE94BBA4-06D1-4D40-A16A-BFD50179D6AC";
+
+            efiModifyPartition.Format           = "FAT32";
+            efiModifyPartition.Label            = "System";
+            efiModifyPartition.Order            = "2";
+            efiModifyPartition.PartitionID     = "2";
+
+            msrModifyPartition.Order            = "3";
+            msrModifyPartition.PartitionID      = "3";
+
+            windowsModifyPartition.Format       = "NTFS";
+            windowsModifyPartition.Label        = "Windows";
+            windowsModifyPartition.Letter       = "C";
+            windowsModifyPartition.Order        = "4";
+            windowsModifyPartition.PartitionID  = "4";
+
+            setupDiskComponent.DiskConfiguration.Disk.ModifyPartitions.ModifyPartition.Add(primaryModifyPartition);
+            setupDiskComponent.DiskConfiguration.Disk.ModifyPartitions.ModifyPartition.Add(efiModifyPartition);
+            setupDiskComponent.DiskConfiguration.Disk.ModifyPartitions.ModifyPartition.Add(msrModifyPartition);
+            setupDiskComponent.DiskConfiguration.Disk.ModifyPartitions.ModifyPartition.Add(windowsModifyPartition);
+
+
+
+            //Add components to settings
+            windowsPESettings.Component.Add(setupUIComponent);
+            windowsPESettings.Component.Add(setupDiskComponent);
+
+            unattendObject.Settings = new System.Collections.Generic.List<Settings>();
+            unattendObject.Settings.Add(windowsPESettings);
+            unattendObject.Settings.Add(oobeSettings);
+            unattendObject.Settings.Add(specializeSettings);
+
+            new XmlSerializer(typeof(WindowsUnattendObject.Unattend)).Serialize(Console.Out, unattendObject);
+        }
+    }
+}
---
+++ WindowsUnattendObject/WindowsAutoUnattend.cs
@@ -0,0 +1,259 @@
+   /* 
+    Licensed under the Apache License, Version 2.0
+    
+    http://www.apache.org/licenses/LICENSE-2.0
+    */
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace WindowsBuilder.WindowsUnattendObject
+{
+	[XmlRoot(ElementName="SetupUILanguage", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class SetupUILanguage {
+		[XmlElement(ElementName="UILanguage", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string UILanguage { get; set; }
+	}
+
+	[XmlRoot(ElementName="component", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class Component {
+		[XmlElement(ElementName="SetupUILanguage", Namespace="urn:schemas-microsoft-com:unattend")]
+		public SetupUILanguage SetupUILanguage { get; set; }
+		[XmlElement(ElementName="InputLocale", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string InputLocale { get; set; }
+		[XmlElement(ElementName="SystemLocale", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string SystemLocale { get; set; }
+		[XmlElement(ElementName="UILanguage", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string UILanguage { get; set; }
+		[XmlElement(ElementName="UserLocale", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string UserLocale { get; set; }
+		[XmlAttribute(AttributeName="name")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName="processorArchitecture")]
+		public string ProcessorArchitecture { get; set; }
+		[XmlAttribute(AttributeName="publicKeyToken")]
+		public string PublicKeyToken { get; set; }
+		[XmlAttribute(AttributeName="language")]
+		public string Language { get; set; }
+		[XmlAttribute(AttributeName="versionScope")]
+		public string VersionScope { get; set; }
+		[XmlAttribute(AttributeName="wcm", Namespace="http://www.w3.org/2000/xmlns/")]
+		public string Wcm { get; set; }
+		[XmlAttribute(AttributeName="xsi", Namespace="http://www.w3.org/2000/xmlns/")]
+		public string Xsi { get; set; }
+		[XmlElement(ElementName="DiskConfiguration", Namespace="urn:schemas-microsoft-com:unattend")]
+		public DiskConfiguration DiskConfiguration { get; set; }
+		[XmlElement(ElementName="ImageInstall", Namespace="urn:schemas-microsoft-com:unattend")]
+		public ImageInstall ImageInstall { get; set; }
+		[XmlElement(ElementName="UserData", Namespace="urn:schemas-microsoft-com:unattend")]
+		public UserData UserData { get; set; }
+		[XmlElement(ElementName="OOBE", Namespace="urn:schemas-microsoft-com:unattend")]
+		public OOBE OOBE { get; set; }
+		[XmlElement(ElementName="UserAccounts", Namespace="urn:schemas-microsoft-com:unattend")]
+		public UserAccounts UserAccounts { get; set; }
+		[XmlElement(ElementName="fDenyTSConnections", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string FDenyTSConnections { get; set; }
+		[XmlElement(ElementName="FirewallGroups", Namespace="urn:schemas-microsoft-com:unattend")]
+		public FirewallGroups FirewallGroups { get; set; }
+		[XmlElement(ElementName="UserAuthentication", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string UserAuthentication { get; set; }
+	}
+
+	[XmlRoot(ElementName="CreatePartition", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class CreatePartition {
+		[XmlElement(ElementName="Order", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Order { get; set; }
+		[XmlElement(ElementName="Size", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Size { get; set; }
+		[XmlElement(ElementName="Type", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Type { get; set; }
+		[XmlAttribute(AttributeName="action", Namespace="http://schemas.microsoft.com/WMIConfig/2002/State")]
+		public string Action { get; set; }
+		[XmlElement(ElementName="Extend", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Extend { get; set; }
+	}
+
+	[XmlRoot(ElementName="CreatePartitions", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class CreatePartitions {
+		[XmlElement(ElementName="CreatePartition", Namespace="urn:schemas-microsoft-com:unattend")]
+		public List<CreatePartition> CreatePartition { get; set; }
+	}
+
+	[XmlRoot(ElementName="ModifyPartition", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class ModifyPartition {
+		[XmlElement(ElementName="Format", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Format { get; set; }
+		[XmlElement(ElementName="Order", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Order { get; set; }
+		[XmlElement(ElementName="PartitionID", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string PartitionID { get; set; }
+		[XmlElement(ElementName="TypeID", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string TypeID { get; set; }
+		[XmlElement(ElementName="Label", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Label { get; set; }
+		[XmlAttribute(AttributeName="action", Namespace="http://schemas.microsoft.com/WMIConfig/2002/State")]
+		public string Action { get; set; }
+		[XmlElement(ElementName="Letter", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Letter { get; set; }
+	}
+
+	[XmlRoot(ElementName="ModifyPartitions", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class ModifyPartitions {
+		[XmlElement(ElementName="ModifyPartition", Namespace="urn:schemas-microsoft-com:unattend")]
+		public List<ModifyPartition> ModifyPartition { get; set; }
+	}
+
+	[XmlRoot(ElementName="Disk", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class Disk {
+		[XmlElement(ElementName="CreatePartitions", Namespace="urn:schemas-microsoft-com:unattend")]
+		public CreatePartitions CreatePartitions { get; set; }
+		[XmlElement(ElementName="ModifyPartitions", Namespace="urn:schemas-microsoft-com:unattend")]
+		public ModifyPartitions ModifyPartitions { get; set; }
+		[XmlElement(ElementName="DiskID", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string DiskID { get; set; }
+		[XmlElement(ElementName="WillWipeDisk", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string WillWipeDisk { get; set; }
+		[XmlAttribute(AttributeName="action", Namespace="http://schemas.microsoft.com/WMIConfig/2002/State")]
+		public string Action { get; set; }
+	}
+
+	[XmlRoot(ElementName="DiskConfiguration", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class DiskConfiguration {
+		[XmlElement(ElementName="Disk", Namespace="urn:schemas-microsoft-com:unattend")]
+		public Disk Disk { get; set; }
+		[XmlElement(ElementName="WillShowUI", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string WillShowUI { get; set; }
+	}
+
+	[XmlRoot(ElementName="InstallTo", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class InstallTo {
+		[XmlElement(ElementName="DiskID", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string DiskID { get; set; }
+		[XmlElement(ElementName="PartitionID", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string PartitionID { get; set; }
+	}
+
+	[XmlRoot(ElementName="OSImage", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class OSImage {
+		[XmlElement(ElementName="InstallTo", Namespace="urn:schemas-microsoft-com:unattend")]
+		public InstallTo InstallTo { get; set; }
+	}
+
+	[XmlRoot(ElementName="ImageInstall", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class ImageInstall {
+		[XmlElement(ElementName="OSImage", Namespace="urn:schemas-microsoft-com:unattend")]
+		public OSImage OSImage { get; set; }
+	}
+
+	[XmlRoot(ElementName="ProductKey", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class ProductKey {
+		[XmlElement(ElementName="Key", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Key { get; set; }
+	}
+
+	[XmlRoot(ElementName="UserData", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class UserData {
+		[XmlElement(ElementName="ProductKey", Namespace="urn:schemas-microsoft-com:unattend")]
+		public ProductKey ProductKey { get; set; }
+		[XmlElement(ElementName="AcceptEula", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string AcceptEula { get; set; }
+		[XmlElement(ElementName="Organization", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Organization { get; set; }
+	}
+
+	[XmlRoot(ElementName="settings", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class Settings {
+		[XmlElement(ElementName="component", Namespace="urn:schemas-microsoft-com:unattend")]
+		public List<Component> Component { get; set; }
+		[XmlAttribute(AttributeName="pass")]
+		public string Pass { get; set; }
+	}
+
+	[XmlRoot(ElementName="OOBE", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class OOBE {
+		[XmlElement(ElementName="HideEULAPage", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string HideEULAPage { get; set; }
+		[XmlElement(ElementName="HideOEMRegistrationScreen", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string HideOEMRegistrationScreen { get; set; }
+		[XmlElement(ElementName="HideOnlineAccountScreens", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string HideOnlineAccountScreens { get; set; }
+		[XmlElement(ElementName="HideWirelessSetupInOOBE", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string HideWirelessSetupInOOBE { get; set; }
+		[XmlElement(ElementName="ProtectYourPC", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string ProtectYourPC { get; set; }
+	}
+
+	[XmlRoot(ElementName="Password", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class Password {
+		[XmlElement(ElementName="Value", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Value { get; set; }
+		[XmlElement(ElementName="PlainText", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string PlainText { get; set; }
+	}
+
+	[XmlRoot(ElementName="LocalAccount", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class LocalAccount {
+		[XmlElement(ElementName="Password", Namespace="urn:schemas-microsoft-com:unattend")]
+		public Password Password { get; set; }
+		[XmlElement(ElementName="Description", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Description { get; set; }
+		[XmlElement(ElementName="DisplayName", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string DisplayName { get; set; }
+		[XmlElement(ElementName="Group", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Group { get; set; }
+		[XmlElement(ElementName="Name", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Name { get; set; }
+		[XmlAttribute(AttributeName="action", Namespace="http://schemas.microsoft.com/WMIConfig/2002/State")]
+		public string Action { get; set; }
+	}
+
+	[XmlRoot(ElementName="LocalAccounts", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class LocalAccounts {
+		[XmlElement(ElementName="LocalAccount", Namespace="urn:schemas-microsoft-com:unattend")]
+		public LocalAccount LocalAccount { get; set; }
+	}
+
+	[XmlRoot(ElementName="UserAccounts", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class UserAccounts {
+		[XmlElement(ElementName="LocalAccounts", Namespace="urn:schemas-microsoft-com:unattend")]
+		public LocalAccounts LocalAccounts { get; set; }
+	}
+
+	[XmlRoot(ElementName="FirewallGroup", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class FirewallGroup {
+		[XmlElement(ElementName="Active", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Active { get; set; }
+		[XmlElement(ElementName="Group", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Group { get; set; }
+		[XmlElement(ElementName="Profile", Namespace="urn:schemas-microsoft-com:unattend")]
+		public string Profile { get; set; }
+		[XmlAttribute(AttributeName="action", Namespace="http://schemas.microsoft.com/WMIConfig/2002/State")]
+		public string Action { get; set; }
+		[XmlAttribute(AttributeName="keyValue", Namespace="http://schemas.microsoft.com/WMIConfig/2002/State")]
+		public string KeyValue { get; set; }
+	}
+
+	[XmlRoot(ElementName="FirewallGroups", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class FirewallGroups {
+		[XmlElement(ElementName="FirewallGroup", Namespace="urn:schemas-microsoft-com:unattend")]
+		public FirewallGroup FirewallGroup { get; set; }
+	}
+
+	[XmlRoot(ElementName="offlineImage", Namespace="urn:schemas-microsoft-com:cpi")]
+	public class OfflineImage {
+		[XmlAttribute(AttributeName="source", Namespace="urn:schemas-microsoft-com:cpi")]
+		public string Source { get; set; }
+		[XmlAttribute(AttributeName="cpi", Namespace="http://www.w3.org/2000/xmlns/")]
+		public string Cpi { get; set; }
+	}
+
+	[XmlRoot(ElementName="unattend", Namespace="urn:schemas-microsoft-com:unattend")]
+	public class Unattend {
+		[XmlElement(ElementName="settings", Namespace="urn:schemas-microsoft-com:unattend")]
+		public List<Settings> Settings { get; set; }
+		[XmlElement(ElementName="offlineImage", Namespace="urn:schemas-microsoft-com:cpi")]
+		public OfflineImage OfflineImage { get; set; }
+		[XmlAttribute(AttributeName="xmlns")]
+		public string Xmlns { get; set; }
+	}
+
+}
